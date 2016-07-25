8d5eeefa8bcf227cc103a9f574cfe1eb63f97332
ZestOfLife <jamescook371@gmail.com>
2016-07-25 11:47:46 -0400

    fixes

--- config/config.js
+++ config/config.js
@@ -133,7 +133,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['127.0.0.1', '103.255.112.26'];
+exports.consoleips = ['127.0.0.1'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
@@ -341,7 +341,6 @@ exports.grouplist = [
 		rangeban: true,
 		tournamentsmanagement: true,
 		declare: true,
-		
 		alts: '$u',
 		modchatjrmod: true,
 	},
